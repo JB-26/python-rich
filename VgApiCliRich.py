0ea60ef42cd5d1318c1369187890537fa48499ef
Joshua Blewitt <joshblewitt@protonmail.com>
2021-11-30 01:58:46 +0000

    Init commit

---
+++ VgApiCliRich.py
@@ -0,0 +1,12 @@
+#importing libraries
+import requests
+import json
+from rich.console import Console
+from rich import inspect
+
+#new console object
+console = Console()
+
+#inspect(console, methods=True)
+
+console.print("Welcome to the Video Game API!", style="bold cyan")
